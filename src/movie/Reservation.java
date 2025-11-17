a10b6e402d9642b3d50ec3359c5043a576a78e8a
seoyk <seobear95@gmail.com>
2025-11-17 11:53:55 +0900

    내 이름 추가

--- src/movie/Reservation.java
+++ src/movie/Reservation.java
@@ -2,9 +2,10 @@ package movie;
 
 public class Reservation {
 
-    public boolean reserve(Movie movie, int count) {
+    public boolean reserve11(Movie movie, int count) {
         if (movie.reserveSeat(count)) {
             System.out.println("✅ 예매 성공: " + movie.getTitle() + ", 수량: " + count);
+            System.out.println("서연경");
             return true;
         } else {
             System.out.println("❌ 예매 실패: 좌석 부족");
